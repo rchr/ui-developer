b1a2b0a888b89e826b5ef4622bafe35e938fd5ec
Mike Taylor <mike@indexdata.com>
2017-06-07 15:41:19 +0100

    Rework using a local subcomponent Entry to reduce repetition

--- settings/Configuration.js
+++ settings/Configuration.js
@@ -8,6 +8,36 @@ import Pane from '@folio/stripes-components/lib/Pane';
 import TextField from '@folio/stripes-components/lib/TextField';
 import Checkbox from '@folio/stripes-components/lib/Checkbox';
 
+
+const Entry = (props) => {
+  const { htmlFor, caption, value, onChange, isBool } = props;
+  const fullFor = `dev-settings-config-${htmlFor}`;
+  return (
+    <Row>
+      <Col xs={12}>
+        <label htmlFor={fullFor}>{caption}</label>
+        {isBool ?
+          <Checkbox id="{fullFor}" checked={value} onChange={onChange} />
+          :
+          <TextField id={fullFor} value={value} onChange={onChange} />
+        }
+      </Col>
+    </Row>
+  );
+};
+
+Entry.propTypes = {
+  htmlFor: PropTypes.string.isRequired,
+  caption: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool,
+  ]).isRequired,
+  onChange: PropTypes.func.isRequired,
+  isBool: PropTypes.bool,
+};
+
+
 class Configuration extends React.Component {
   static propTypes = {
     label: PropTypes.string.isRequired,
@@ -24,13 +54,13 @@ class Configuration extends React.Component {
     }).isRequired,
   };
 
-  onChange(e, path, caption, isBool) {
+  onChange(e, path, isBool) {
     const stripes = this.context.stripes;
     const lastComponent = path.pop();
     const val = isBool ? e.target.checked : e.target.value;
 
     _.get(stripes, path)[lastComponent] = val;
-    stripes.logger.log('action', `changed ${caption} (${path.join('.')}.${lastComponent}) to '${val}'`);
+    stripes.logger.log('action', `changed ${path.join('.')}.${lastComponent} to '${val}'`);
     this.forceUpdate();
   }
 
@@ -58,62 +88,14 @@ class Configuration extends React.Component {
     return (
       <Pane defaultWidth="fill" fluidContentWidth paneTitle={this.props.label}>
         <HotKeys keyMap={globalKeyMap} handlers={handlers}>
-          <Row>
-            <Col xs={12}>
-              <label htmlFor="settingLoggingCategories">Logging categories</label>
-              <TextField
-                value={stripes.logger.categories}
-                onChange={e => this.onChange(e, ['logger', 'categories'], 'logging categories')}
-              />
-            </Col>
-          </Row>
+          <Entry htmlFor="1" caption="Logging categories" value={stripes.logger.categories} onChange={e => this.onChange(e, ['logger', 'categories'])} />
           <hr />
-          <Row>
-            <Col xs={12}>
-              <label htmlFor="settingAutoLoginUsername">Auto-login username</label>
-              <TextField
-                value={stripes.config.autoLogin.username}
-                onChange={e => this.onChange(e, ['config', 'autoLogin', 'username'], 'autoLogin username')}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <label htmlFor="settingAutoLoginPassword">Auto-login password</label>
-              <TextField
-                value={stripes.config.autoLogin.password}
-                onChange={e => this.onChange(e, ['config', 'autoLogin', 'password'], 'autoLogin password')}
-              />
-            </Col>
-          </Row>
+          <Entry htmlFor="2" caption="Auto-login username" value={stripes.config.autoLogin.username} onChange={e => this.onChange(e, ['config', 'autoLogin', 'username'])} />
+          <Entry htmlFor="3" caption="Auto-login password" value={stripes.config.autoLogin.password} onChange={e => this.onChange(e, ['config', 'autoLogin', 'password'])} />
           <hr />
-          <Row>
-            <Col xs={12}>
-              <label htmlFor="settingShowPerms">Show permissions in user menu?</label>
-              <Checkbox
-                checked={stripes.config.showPerms}
-                onChange={e => this.onChange(e, ['config', 'showPerms'], 'show permissions', true)}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <label htmlFor="settingListInvisiblePerms">List "invisible" permissions in add-perm menus?</label>
-              <Checkbox
-                checked={stripes.config.listInvisiblePerms}
-                onChange={e => this.onChange(e, ['config', 'listInvisiblePerms'], 'list invisible permissions', true)}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <label htmlFor="settingHasAllPerms">Act as though user had all permissions</label>
-              <Checkbox
-                checked={stripes.config.hasAllPerms}
-                onChange={e => this.onChange(e, ['config', 'hasAllPerms'], 'has all permissions', true)}
-              />
-            </Col>
-          </Row>
+          <Entry htmlFor="4" caption="Show permissions in user menu?" value={stripes.config.showPerms} onChange={e => this.onChange(e, ['config', 'showPerms'], true)} isBool />
+          <Entry htmlFor="5" caption="List &quot;invisible&quot; permissions in add-perm menus??" value={stripes.config.listInvisiblePerms} onChange={e => this.onChange(e, ['config', 'listInvisiblePerms'], true)} isBool />
+          <Entry htmlFor="6" caption="Act as though user has all permissions" value={stripes.config.hasAllPerms} onChange={e => this.onChange(e, ['config', 'hasAllPerms'], true)} isBool />
         </HotKeys>
       </Pane>
     );
