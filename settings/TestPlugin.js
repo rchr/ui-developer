ef23ac7ee346628ea5104d4ab3d5d140c3835b0d
Mike Taylor <mike@indexdata.com>
2017-06-12 00:06:57 +0100

    Add caption on how to change selected plugin

--- settings/TestPlugin.js
+++ settings/TestPlugin.js
@@ -1,15 +1,19 @@
 // We have to remove node_modules/react to avoid having multiple copies loaded.
 // eslint-disable-next-line import/no-unresolved
 import React, { PropTypes } from 'react';
+import Link from 'react-router-dom/Link';
 import Pane from '@folio/stripes-components/lib/Pane';
 import Pluggable from '@folio/stripes-components/lib/Pluggable';
 
 function TestPlugin(props) {
   return (
     <Pane defaultWidth="fill" fluidContentWidth paneTitle={props.label}>
-      <Pluggable type="markdown-editor">
-        <div style={{ background: 'red' }}>[Markdown editor goes here]</div>
-      </Pluggable>
+      <div>
+        <Pluggable type="markdown-editor">
+          <div style={{ background: 'red' }}>[Markdown editor goes here]</div>
+        </Pluggable>
+        <p>To change the preferred plugin, go <Link to="/settings/organization/plugins">here</Link>.</p>
+      </div>
     </Pane>
   );
 }
