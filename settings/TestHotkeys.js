89a224617e969f47300fae4209f20e8d346cb0ef
Zak Burke <zburke@cornell.edu>
2017-10-12 12:32:03 -0400

    Use PropTypes, not React.PropTypes. Refs STRIPES-427

--- settings/TestHotkeys.js
+++ settings/TestHotkeys.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { HotKeys } from '@folio/stripes-components/lib/HotKeys';
 import Pane from '@folio/stripes-components/lib/Pane';
 
