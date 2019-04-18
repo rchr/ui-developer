2101313d000c46a98c5b394d219a524744f7d449
Peter Murray <jester@dltj.org>
2019-04-18 17:42:52 -0400

    Add Spanish and Spanish (Latin America)
    FIXES UIORG-162

--- settings/Locale.js
+++ settings/Locale.js
@@ -20,6 +20,8 @@ const Locale = (props) => {
     { value: 'it-IT', label: 'Italian - Italy' },
     { value: 'pt-BR', label: 'Portuguese - Brazil' },
     { value: 'pt-PT', label: 'Portuguese - Portugal' },
+    { value: 'es', label: 'Spanish' },
+    { value: 'es-419', label: 'Spanish - Latin America' },
     { value: 'es-ES', label: 'Spanish - Spain' },
   ];
 
