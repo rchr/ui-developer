97019c26d0a5a2b8d2dc2e91e1ff2a385afa6f87
Zak Burke <zburke@users.noreply.github.com>
2020-05-06 07:25:32 -0400

    links are nice (#114)

--- settings/OkapiPaths.js
+++ settings/OkapiPaths.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
+import { Link } from 'react-router-dom';
 
 import { stripesConnect } from '@folio/stripes/core';
-
-
 import {
   Pane,
 } from '@folio/stripes/components';
@@ -45,18 +44,28 @@ class OkapiPaths extends React.Component {
     };
   }
 
+  /**
+   * read the list of modules from props.stripes.discovery.modules, then
+   * request /_/proxy/modules/$i and parse the result to find the list of
+   * paths supported by each implementation and store the result in state.
+   */
   componentDidMount() {
     const { mutator } = this.props;
 
     const modules = get(this.props.stripes, ['discovery', 'modules']) || {};
 
     const paths = this.state.paths;
-    Object.keys(modules).forEach(m => {
-      mutator.moduleDetails.GET({ path: `_/proxy/modules/${m}` }).then(res => {
+    Object.keys(modules).forEach(impl => {
+      mutator.moduleDetails.GET({ path: `_/proxy/modules/${impl}` }).then(res => {
+        const iface = this.implToInterface(impl);
         if (res.provides) {
           res.provides.forEach(i => {
             i.handlers.forEach(handler => {
-              paths[handler.pathPattern] = m;
+              paths[handler.pathPattern] = {
+                iface,
+                impl,
+                ramlsLink: <Link to={`//github.com/folio-org/${iface}/tree/master/ramls`}>{iface}</Link>,
+              };
             });
           });
         }
@@ -65,12 +74,35 @@ class OkapiPaths extends React.Component {
     });
   }
 
+  /**
+   * map a module implementation string to an interface, hopefully.
+   * given a string like "mod-users-16.2.0-SNAPSHOT.127" return "mod-users"
+   */
+  implToInterface(impl) {
+    const iface = impl.match(/^(.*)-[0-9].*/);
+    return iface[1] ? iface[1] : '';
+  }
+
+  /**
+   * iterate through this.state.paths to find those matching this.state.filter
+   * and return an array of nicely formatted <li> elements
+   */
   showPaths = () => {
+    // dear ESLint: I just want you to know that
+    //   <li key={path}>{path} = {this.state.paths[path].ramlsLink}</li>);
+    // is SO MUCH CLEARER than
+    //         <li key={path}>
+    //            {path}
+    //            =
+    //            {this.state.paths[path].ramlsLink}
+    //          </li>
+    // AND it actually formats the way I want, with spaces around the equals.
+    // Sometimes, and I hate to tell it to you this way, you suck at your job.
     return Object
       .keys(this.state.paths)
       .sort()
       .filter(path => path.indexOf(this.state.filter) >= 0)
-      .map(path => <li key={path}>{path} = {this.state.paths[path]}</li>);
+      .map(path => <li key={path}>{path} = {this.state.paths[path].ramlsLink}</li>); // eslint-disable-line
   }
 
   handleFilter = (event) => {
