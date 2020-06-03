a498fc746b4d6cf3fc64d880059bb43b4d2cce9b
Zak Burke <zburke@users.noreply.github.com>
2020-06-03 08:40:40 -0400

    use intl.formatDisplayName to format locales (#128)
    Instead of using locally managed translations for locale names, use
    `intl.formatDisplayName()`. Additional details at
    https://formatjs.io/docs/react-intl/api#formatdisplayname.
    
    ```
    // When locale is `en`
    formatDisplayName('zh-Hans-SG'); //=> Simplified Chinese (Singapore)
    // When locale is `zh`
    formatDisplayName('zh-Hans-SG'); //=> 简体中文（新加坡）
    
    // When locale is `en`...
    // ISO-15924 four letters script code to localized display name
    formatDisplayName('Deva', {type: 'script'}); //=> Devanagari
    // ISO-4217 currency code to localized display name
    formatDisplayName('CNY', {type: 'currency'}); //=> Chinese Yuan
    // ISO-3166 two letters region code to localized display name
    formatDisplayName('UN', {type: 'region'}); //=> United Nations
    ```

--- src/settings/Locale.js
+++ src/settings/Locale.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import { useIntl, createIntl, createIntlCache } from 'react-intl';
 
 import {
   Pane,
@@ -9,29 +9,50 @@ import {
 } from '@folio/stripes/components';
 
 const Locale = (props) => {
+  const intl = useIntl();
+
   const locales = [
-    { value: 'ar-AR', label: <FormattedMessage id="ui-developer.sessionLocale.ar-AR" /> },
-    { value: 'zh-CN', label: <FormattedMessage id="ui-developer.sessionLocale.zh-CN" /> },
-    { value: 'zh-TW', label: <FormattedMessage id="ui-developer.sessionLocale.zh-TW" /> },
-    { value: 'da-DK', label: <FormattedMessage id="ui-developer.sessionLocale.da-DK" /> },
-    { value: 'en-GB', label: <FormattedMessage id="ui-developer.sessionLocale.en-GB" /> },
-    { value: 'en-SE', label: <FormattedMessage id="ui-developer.sessionLocale.en-SE" /> },
-    { value: 'en-US', label: <FormattedMessage id="ui-developer.sessionLocale.en-US" /> },
-    { value: 'fr-FR', label: <FormattedMessage id="ui-developer.sessionLocale.fr-FR" /> },
-    { value: 'de-DE', label: <FormattedMessage id="ui-developer.sessionLocale.de-DE" /> },
-    { value: 'he', label: <FormattedMessage id="ui-developer.sessionLocale.he" /> },
-    { value: 'hu-HU', label: <FormattedMessage id="ui-developer.sessionLocale.hu-HU" /> },
-    { value: 'ja', label: <FormattedMessage id="ui-developer.sessionLocale.ja" /> },
-    { value: 'it-IT', label: <FormattedMessage id="ui-developer.sessionLocale.it-IT" /> },
-    { value: 'pt-BR', label: <FormattedMessage id="ui-developer.sessionLocale.pt-BR" /> },
-    { value: 'pt-PT', label: <FormattedMessage id="ui-developer.sessionLocale.pt-PT" /> },
-    { value: 'ru', label: <FormattedMessage id="ui-developer.sessionLocale.ru" /> },
-    { value: 'es', label: <FormattedMessage id="ui-developer.sessionLocale.es" /> },
-    { value: 'es-419', label: <FormattedMessage id="ui-developer.sessionLocale.es-419" /> },
-    { value: 'es-ES', label: <FormattedMessage id="ui-developer.sessionLocale.es-ES" /> },
-    { value: 'ur', label: <FormattedMessage id="ui-developer.sessionLocale.ur" /> },
+    { value: 'ar', label: '' },
+    { value: 'zh-CN', label: '' },
+    { value: 'zh-TW', label: '' },
+    { value: 'da-DK', label: '' },
+    { value: 'en-GB', label: '' },
+    { value: 'en-SE', label: '' },
+    { value: 'en-US', label: '' },
+    { value: 'fr-FR', label: '' },
+    { value: 'de-DE', label: '' },
+    { value: 'he', label: '' },
+    { value: 'hu-HU', label: '' },
+    { value: 'ja', label: '' },
+    { value: 'it-IT', label: '' },
+    { value: 'pt-BR', label: '' },
+    { value: 'pt-PT', label: '' },
+    { value: 'ru', label: '' },
+    { value: 'es', label: '' },
+    { value: 'es-419', label: '' },
+    { value: 'es-ES', label: '' },
+    { value: 'ur', label: '' },
   ];
 
+  // This is optional but highly recommended
+  // since it prevents memory leak
+  const cache = createIntlCache();
+
+  locales.forEach(locale => {
+    locale.intl = createIntl({
+      locale: locale.value,
+      messages: {}
+    }, cache);
+
+    // label contains language in current locale and in destination locale
+    // e.g. given the current locale is `en` and the keys `ar` and `zh-CN` show:
+    //     Arabic / العربية
+    //     Chinese (China) / 中文（中国）
+    // e.g. given the current locale is `ar` and the keys `ar` and `zh-CN` show:
+    //    العربية / العربية
+    //    الصينية (الصين) / 中文（中国）
+    locale.label = `${intl.formatDisplayName(locale.value)} / ${locale.intl.formatDisplayName(locale.value)}`;
+  });
 
   const setLocale = (locale) => {
     if (locale) props.stripes.setLocale(locale.value);
@@ -44,7 +65,7 @@ const Locale = (props) => {
   return (
     <Pane
       defaultWidth="fill"
-      paneTitle={<FormattedMessage id="ui-developer.sessionLocale.temporarySessionLocale" />}
+      paneTitle={intl.formatMessage({ id: 'ui-developer.sessionLocale.temporarySessionLocale' })}
     >
       <List
         listStyle="bullets"
