15801742c05e60241fdcf25521d44e5715e828a7
Zak Burke <zburke@users.noreply.github.com>
2020-06-13 07:12:14 -0400

    sort that list because duh (#134)

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import { Settings } from '@folio/stripes/smart-components';
 
@@ -16,52 +16,62 @@ import OkapiConfiguration from './OkapiConfiguration';
 const pages = [
   {
     route: 'configuration',
-    label: <FormattedMessage id="ui-developer.configuration" />,
+    labelId: 'ui-developer.configuration',
     component: Configuration,
     perm: 'ui-developer.settings.configuration',
   },
   {
     route: 'hotkeys',
-    label: <FormattedMessage id="ui-developer.hotkeys" />,
+    labelId: 'ui-developer.hotkeys',
     component: TestHotkeys,
     perm: 'ui-developer.settings.hotkeys',
   },
   {
     route: 'token',
-    label: <FormattedMessage id="ui-developer.setToken" />,
+    labelId: 'ui-developer.setToken',
     component: Token,
     perm: 'ui-developer.settings.token',
   },
   {
     route: 'locale',
-    label: <FormattedMessage id="ui-developer.sessionLocale" />,
+    labelId: 'ui-developer.sessionLocale',
     component: Locale,
     perm: 'ui-developer.settings.locale',
   },
   {
     route: 'okapi-paths',
-    label: <FormattedMessage id="ui-developer.okapiPaths" />,
+    labelId: 'ui-developer.okapiPaths',
     component: OkapiPaths,
   },
   {
     route: 'can-i-use',
-    label: <FormattedMessage id="ui-developer.canIUse" />,
+    labelId: 'ui-developer.canIUse',
     component: CanIUse,
   },
   {
     route: 'folio-babies',
-    label: <FormattedMessage id="ui-developer.folioBabies" />,
+    labelId: 'ui-developer.folioBabies',
     component: FolioBabies,
   },
   {
     route: 'okapi-configuration',
-    label: <FormattedMessage id="ui-developer.okapiConfigurationEntries" />,
+    labelId: 'ui-developer.okapiConfigurationEntries',
     component: OkapiConfiguration,
     perm: 'ui-developer.settings.okapiConfiguration',
   },
 ];
 
 const DeveloperSettings = (props) => {
+  const intl = useIntl();
+
+  pages.forEach(p => {
+    p.label = intl.formatMessage({ id: p.labelId });
+  });
+
+  pages.sort((a, b) => {
+    return a.label.localeCompare(b.label);
+  });
+
   return <Settings {...props} pages={pages} paneTitle={<FormattedMessage id="ui-developer.meta.title" />} />;
 };
 
