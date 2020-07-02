6195827af9f49cba0303261b43f7fdabd39352d3
Zak Burke <zburke@users.noreply.github.com>
2020-07-02 14:19:54 -0400

    display decoded token (#144)

--- src/settings/Token.js
+++ src/settings/Token.js
@@ -7,26 +7,72 @@ import { stripesShape } from '@folio/stripes/core';
 import { ConfigForm } from '@folio/stripes/smart-components';
 import { Col, Row, TextField } from '@folio/stripes/components';
 
-
+/**
+ * Display decoded JWT token. Set a new token.
+ */
 class Token extends React.Component {
   static propTypes = {
     label: PropTypes.string.isRequired,
     stripes: stripesShape.isRequired,
   };
 
-  constructor() {
-    super();
-    this.onSave = this.onSave.bind(this);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      raw: props.stripes.store.getState().okapi.token,
+      cooked: {},
+      error: '',
+    };
   }
 
-  onSave(data) {
+  /**
+   * decode the original token
+   */
+  componentDidMount = () => {
+    this.setState((state) => ({
+      cooked: this.parseToken(state.raw),
+    }));
+  }
+
+  onSave = (data) => {
     const stripes = this.props.stripes;
     stripes.setToken(data.token);
   }
 
+  /**
+   * decode a JWT token and save it in state.cooked. if decoding fails,
+   * write an error to state.error.
+   *
+   * verbatim from https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
+   */
+  parseToken = (t) => {
+    let parsed = {};
+    if (t) {
+      try {
+        const base64Url = t.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        parsed = JSON.parse(jsonPayload);
+        this.setState({ error: '', cooked: parsed });
+      } catch (e) {
+        this.setState({ error: 'Malformed token', cooked: '' });
+      }
+    }
+  }
+
+  /**
+   * parse the new value.
+   */
+  handleTokenChange = (event, newValue) => {
+    this.parseToken(newValue);
+  }
+
   render() {
-    const stripes = this.props.stripes;
-    const token = stripes.store.getState().okapi.token;
+    const token = this.state.raw;
 
     return (
       <div style={{ width: '100%' }}>
@@ -38,7 +84,21 @@ class Token extends React.Component {
                 id="token"
                 name="token"
                 label={<FormattedMessage id="ui-developer.setToken.authenticationToken" />}
+                onChange={this.handleTokenChange}
               />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <dl>
+                {this.state.cooked && Object.entries(this.state.cooked).map(([k, v]) => (
+                  <>
+                    <dt key={k}>{k}</dt>
+                    <dd>{v}</dd>
+                  </>
+                ))}
+              </dl>
+              {this.state.error && <div>{this.state.error}</div>}
             </Col>
           </Row>
         </ConfigForm>
